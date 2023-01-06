b1ebcecd1e6135bfff757ac2bec0ec6d3e89c674
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-06 16:51:24 +0700

    feat(dynamic-routes): add
    + basics/dynamic-routes
    + Create dynamic-route with getStaticPaths & implement concept of paths
      and getStaticProps follow the dynamic params.

---
+++ basics/dynamic-routes/pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hey I am <b>Expitc</b> known as <i><b>Tri</b></i>. Nice to meet you Sir!</p>
+        <center className={utilStyles.fade}><b><i>THEY DON'T KNOW ME SON!</i></b></center>
+        <p className={utilStyles.fade}>(This is a sample website - you'll be building a site like this on<br />
+          <a href="https://nextjs.org/learn">Our Next.js tutorial</a>)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
