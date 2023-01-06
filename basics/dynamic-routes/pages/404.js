7efabfa029a4abb3484755d04a47d5d4b0473095
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-06 16:59:39 +0700

    feat(dynamic-route): add costume 404 page
    + dynamic-route/pages/404.js
    + Apply concept & adding costume 404 route.

---
+++ basics/dynamic-routes/pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'black', color: 'whitesmoke' }}>
+      <h1>404 - Page Not Found.</h1>
+      <p>EXPITC</p>
+    </div>
+  )
+}
